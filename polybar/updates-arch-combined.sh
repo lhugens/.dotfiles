93816d23ed1d32252355065da433dfe476bf3b35
lhugens <leonardolobato@protonmail.com>
2020-05-19 21:34:04 +0000

    update

--- polybar/updates-arch-combined.sh
+++ polybar/updates-arch-combined.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-#if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-#    updates_arch=0
-#fi
+updates=$(yay -Qu 2> /dev/null | wc -l)
 
-if ! updates_aur=$(yay -Qu 2> /dev/null | wc -l); then
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
-# if ! updates_aur=$(rua upgrade --printonly 2> /dev/null | wc -l); then
-    updates_aur=0
-fi
-
-updates=$(("$updates_arch" + "$updates_aur"))
-
-if [ "$updates" -gt 0 ]; then
-    echo "# ${updates} updates"
-else
-    echo ""
-fi
+[[ "$updates" -gt 0 ]] && { echo "${updates} updates"; } || { echo ""; } 
