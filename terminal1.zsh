db0d2d0b38c3862476e4f4e6f5d5a1d374b23bde
Leonardo Hugens <leonardohugens@protonmail.com>
2024-09-30 08:47:36 +0100

    new termnial scripts

--- terminal1.zsh
+++ terminal1.zsh
@@ -1,9 +1,4 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-
-brew install zsh-autosuggestions;
+brew install zsh-autosuggestions zsh-syntax-highlighting;
 
 echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc;
-
-brew install zsh-syntax-highlighting;
-
 echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc;
